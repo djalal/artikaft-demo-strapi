a1040200286375de26bb58b5ff6c9b75c707871c
djalal <djalal@users.noreply.github.com>
2022-04-11 18:20:47 +0200

    fix entrypoint.sh

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -2,9 +2,8 @@
 
 persistent_folder=src
 echo Mount $persistent_folder directory
+mkdir -p /data/$persistent_folder
 cp -ur /var/www/html/$persistent_folder/* /data/$persistent_folder || true
 rm -rf /var/www/html/$persistent_folder && \
-  mkdir -p /data/$persistent_folder && \
   ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
   chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
-
