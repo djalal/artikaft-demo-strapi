2d8474bbdbcf2fefff8f9e17753f16cdecf9998e
djalal <djalal@users.noreply.github.com>
2022-04-14 11:10:25 +0200

    add uploads to persistent disk

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -8,5 +8,12 @@ rm -rf /var/www/html/$persistent_folder && \
   ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
   chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
 
-cd /data/$persistent_folder && npm link @strapi/strapi && cd /var/www/html
+persistent_folder=public/uploads
+echo Mount $persistent_folder directory
+mkdir -p /data/$persistent_folder
+cp -ur /var/www/html/$persistent_folder/* /data/$persistent_folder || true
+rm -rf /var/www/html/$persistent_folder && \
+  ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
+  chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
 
+cd /data/$persistent_folder && npm link @strapi/strapi && cd /var/www/html
